e5a6078fe8207064edddb6d66770e82935ce57ee
Niiik27 <niiik494@gmail.com>
2023-06-14 16:11:52 +0400

    Прикол с опросом

--- Home/test/main.py
+++ Home/test/main.py
@@ -1,3 +1,62 @@
+checked_users = {}
+next_user = 0
+
+lessonDays = ["Четверг","Пятница"]
+users = [
+    "Денис Игоревич",
+    "Меньшиков",
+    "Выборнов",
+    "Курочкин",
+    "Мухитов",
+    "Попов",
+    "Чепаксин",
+    "Карпов",
+    "Соколов",
+    "Захаров",
+    "Казаков",
+    "Хисамов",
+]
+
+if len(checked_users) <= len(users):
+
+    userSurname = ""
+    while userSurname not in users:
+        userSurname = input("Если вы студент, то введите свою фамилию, если преподаватель, то имя отчество> ")
+        if userSurname in users:
+            break
+        print ("Нет такого пользователя! ")
+    if users.index(userSurname)>next_user:
+        print("Ждите своей очереди. Вам должны прислать две строки, которыми вы замените первые две строчки кода.")
+        exit(0)
+
+    days_str = ""
+    for i in range(len(lessonDays)):
+        days_str+=f"{i+1} - {lessonDays[i]}\n"
+
+    choice = int(input(f"{users[next_user]} выберите день недели:\n{days_str}>"))-1
+    checked_users[users[next_user]] = choice
+    if next_user + 1 < len(users):
+        print(f"{users[next_user]} отправте эти строчки {users[next_user+1]}у для замены первых двух строчек кода:")
+        next_user += 1
+        print(f"checked_users = {checked_users}")
+        print(f"next_user = {next_user}")
+    else:
+        forthday_num = 0
+        frighday_num = 0
+        next_lesson = ""
+        for key in checked_users:
+            if checked_users[key] == 0:
+                forthday_num += 1
+            else:
+                frighday_num += 1
+        next_lesson = "Четверг" if forthday_num > frighday_num else "Пятница"
+        print("Опубликуйте результат: ")
+        print(f"Следующее занятие будет в {next_lesson}")
+exit(0)
+
+
+
+
 
 list_1 = [3,3,1,3,7,9,5,4,2,8,6]
 list_2 = [12,3,4,7,13,6,2,1,0,4]
