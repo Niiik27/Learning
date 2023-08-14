f03fc68c5ec43973e027790e68ce97a2669bf0e5
Niiik27 <menshikovn@yandex.ru>
2023-08-14 23:53:40 +0400

    Промежуточное сохранение дз1

--- TOP/js/dz1.js
+++ TOP/js/dz1.js
@@ -63,6 +63,11 @@
 // }
 // alert(`Результат ${result}`)
 
+
+
+
+
+
 // alert("Упражнение №1 - определить статус по возрасту");
 // let user_age = +prompt("Сколько вам лет? ")
 // if (0 < user_age && user_age < 12){
@@ -77,29 +82,173 @@
 //     alert("Вас нет")}
 
 
-alert("Упражнение №2 - вывести символ shif+цифра - подходит match/case")
-pressed_key = prompt("Нажмити цифру на клавиатуре ");
-switch (pressed_key){
-    case "1":
-        alert("!");
-    case "2":
-        alert("@");
-    case "3":
-        alert("#");
-    case "4":
-        alert("$");
-    case "5":
-        alert("%");
-    case "6":
-        alert("^");
-    case "7":
-        alert("&");
-    case "8":
-        alert("*");
-    case "9":
-        alert("(");
-    case "0":
-        alert("0");
-    default:
-        alert("Не та кнопка");
-    }
+// alert("Упражнение №2 - вывести символ shif+цифра")
+// pressed_key = prompt("Нажмити цифру на клавиатуре ");
+// switch (pressed_key){
+//     case "!":
+//         alert("1");
+//         break;
+//     case "@":
+//     case "\"":
+//         alert("2");
+//         break;
+//     case "#":
+//     case "№":
+//         alert("3");
+//         break;
+//     case "$":
+//     case ";":
+//         alert("4");
+//         break;
+//     case "%":
+//         alert("5");
+//         break;
+//     case "^":
+//     case ":":
+//         alert("6");
+//         break;
+//     case "&":
+//     case "?":
+//         alert("7");
+//         break;
+//     case "*":
+//         alert("8");
+//         break;
+//     case "(":
+//         alert("9");
+//         break;
+//     case ")":
+//         alert("0");
+//         break;
+//     default:
+//         alert("Не та кнопка");
+//         break;
+//     }
+
+
+// alert("Упражнение №3 - проверить - есть ли в трехзначном числе одинаковые цифры");
+
+// let input_string = prompt("Введите трехзначное число ");
+// let num = Number(input_string);
+
+
+// let order = input_string.length-1
+// let first = Math.floor(num/(10**order));
+// num = num%(10**order)
+
+// order = order-1
+// let second = Math.floor(num/(10**order));
+// num = num%(10**order)
+
+// order = order-1
+// let third = Math.floor(num/(10**order));
+// num = num%(10**order)
+
+// let result =  first!=second && second !=third && first!=third? "Нет повторяющихся цифр": "Есть повторения"
+
+// alert(result)
+
+
+// alert("Упражнение №4 - определить - является ли год високосным")
+// let year = Number(prompt("Введите год "))
+// if (year%400==0){
+//     alert("Високосный")
+// }
+// else if (year%100==0){
+//     alert("Обычный")
+// }
+// else if (year%4==0){
+//     alert("Високосный")
+// }
+// else{
+//     alert("Обычный")
+// }
+
+// alert("Упражнение №5 - проверить число на палиндром")
+// let input_string = prompt("Введите пятизначное число ")
+// let num = Number(input_string)
+// let result = "Это не плалиндром!"
+
+// let order = input_string.length-1
+// let first_right = num - Math.floor(num/10)*10
+
+
+// let first_left = Math.floor(num/(10**order))
+// num = Math.floor(num%(10**order)/10)
+// order = order-1
+
+// if (first_right == first_left)
+// {
+//     order = order-1
+//     first_right = num - Math.floor(num/10)*10
+//     first_left = Math.floor(num/(10**order))
+//     num = Math.floor(num%(10**order)/10)
+//     order = order-1//Теперь число теряет по 2 цифры
+//     if (first_right == first_left){
+//         order = order-1
+//         first_right = num - Math.floor(num/10)*10
+//         first_left = Math.floor(num/(10**order))
+//         num = Math.floor(num%(10**order)/10)
+//         order = order-1
+//         if (first_right == first_left){
+//             result = "Это ПАЛИНДРОМ!"}
+//         else{
+//             result = "Это не плалиндром!"}
+//         }
+//     else{
+//         result = "Это не плалиндром!"
+//     }
+// }
+// alert(result)
+
+
+// alert("Упражнение №6 - Сконвертировать доллар в другую валюту")
+// let usd = +prompt("Введите сумму в долларах ")
+// let money = prompt("Во что конвертировать - EUR, UAN, AZN, RU(По умолчанию)? ")
+// let result =  0.0
+// switch (money.toLowerCase())
+// {   case "":
+//         money= "ru"
+//     case "ru":
+//         result =  usd*80.17;
+//         break;
+//     case "eur":
+//         result =  usd*0.92764;
+//         break;
+//     case "uan":
+//         result =  usd*7.06;
+//         break;
+//     case "azn":
+//         result =  usd*1.7;
+//         break;
+//     default:
+//         alert(`В ${money} не конвертирую`);
+//         result =  usd;
+//         money = "USD";
+//         }
+// alert(`${usd} USD = ${result} ${money.toUpperCase()}`)
+
+
+// alert("Упражнение №7 - Сделать скидку")
+// let money = +prompt("Введите сумму покупки ");
+
+// if(money < 0){
+//     alert(`Введите положительное число. Вы ввели ${money}`);
+// }
+// else{
+//     let discount = 0;
+//         if (money >= 200 && money < 300){
+//             discount = 3;
+//         }
+//         else if(money >= 300 && money < 500){
+//             discount = 5;
+//         }
+//         else if(money >= 500){
+//         discount = 7;
+//         }
+//     money = money - (money/100*discount);       
+//     alert(`Цена со скидкой ${discount}% = ${money} рублей`);
+        
+// }
+     
+
