04bdc0c7e94e265f955c59cce963773deddb2c11
Niiik27 <niiik494@gmail.com>
2023-08-31 13:23:48 +0400

    1. Развернул проект с 4 раза. По этому в комите присутствуют приложения проекта. Так вышло - пока разбирался почему не запускается, и что с интерперетатором успел их создать. Теперь нужно их привязать

--- DjangoProjects/DjangoDZ1/registration/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-def registration_view(request):
-    return render(request, template_name='./registration/registration.html')
-
-# Create your views here.
--- DjangoProjects/DjangoDZ1/userprofile/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-def userprofile_view(request):
-    return render(request, template_name='./userprofile/userprofile.html')
-
-# Create your views here.
