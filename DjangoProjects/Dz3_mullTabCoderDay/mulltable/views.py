66e76e1cc6ac5b8d0d25a7a77e057c8988b92ebc
Niiik27 <menshikovn@yandex.ru>
2023-09-11 00:05:04 +0400

    Dz3 Базовая версия

--- DjangoProjects/Dz3_mullTabCoderDay/mulltable/views.py
+++ DjangoProjects/Dz3_mullTabCoderDay/mulltable/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-def mulltabView(request):
+def mulltabView11(request):
     tab_strs = []
     for i in range(2,10):
         for j in range(2, 10):
             a = i*j
             tab_strs.append(f'{i} x {j} = {a}')
         tab_strs.append("*"*10)
+    return render(request, template_name='mulltable/mulltable.html', context={'mull_tab': tab_strs})
+
+
+def mulltabView(request):
+    tab_strs = []
+    for i in range(2,10):
+        var_strs=[]
+        for j in range(2, 10):
+            a = i*j
+            var_strs.append(f'{i} x {j} = {a}')
+        tab_strs.append(var_strs)
     return render(request, template_name='mulltable/mulltable.html', context={'mull_tab': tab_strs})
